234fbc215016d300787e2913f0781d61e8ce7513
Jacky Chiu <jacky.chiu888@gmail.com>
2016-09-29 20:35:56 -0400

    Added input exceptions

--- Assignment1/src/Application.java
+++ Assignment1/src/Application.java
@@ -5,24 +5,33 @@ public class Application {
     public static void main(String[] args) {
         ChessGame chessGame = new ChessGame();
         ChessLocation location = new ChessLocation(0, 0);
+        int row, col;
 
         chessGame.getChessBoard().displayBoard();
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the row you want to move to, followed by the column. Enter 'Q' at anytime to stop.");
-        String row = input.nextLine();
-        String col = input.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the row,col to move to in this format: row,col. Enter 'Q' at anytime to stop.");
+        String input = scanner.nextLine();
 
-        while(!row.equals("Q") && !col.equals("Q")){
-            System.out.println("Location selected: (" + row + " , " + col + ")");
+        while(!input.equals("Q")){
+            try {
+                row = Integer.parseInt(input.split(",")[0]);
+                col =Integer.parseInt(input.split(",")[1]);
 
-            location.setRowAndCol(Integer.parseInt(row), Integer.parseInt(col));
-            chessGame.getChessBoard().placePieceAt(chessGame.getPiece(), location);
+                System.out.println("Location selected: (" + row + " , " + col + ")");
 
-            chessGame.getChessBoard().displayBoard();
+                location.setRowAndCol(row, col);
+                chessGame.getChessBoard().placePieceAt(chessGame.getPiece(), location);
 
-            row = input.nextLine();
-            col = input.nextLine();
+                chessGame.getChessBoard().displayBoard();
+
+            } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+                System.out.println("Couldn't parse input, the format: row,col. Also 'Q' to quit.");
+            } catch (NullPointerException e) {
+                System.out.println("NullPointerException :(, GL Debugging");
+                e.printStackTrace();
+            }
+            input = scanner.nextLine();
         }
         System.out.println("Game has ended");
     }
