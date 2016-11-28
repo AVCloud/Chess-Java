8e170cb6a698ef7622d0f43539480e56ac411700
Jacky Chiu <jacky.chiu888@gmail.com>
2016-11-27 21:23:58 -0500

    King checks for threats

--- src/GameEntities/King.java
+++ src/GameEntities/King.java
@@ -2,9 +2,11 @@ package GameEntities;
 
 import Chess.ChessLocation;
 import Chess.ChessGame;
+import java.util.ArrayList;
 
 public class King extends ChessPiece {
     
+    private ArrayList<ChessPiece> threateningPieces;
     /**
      * Creates a King piece.
      * @param owner Owner string.
@@ -18,6 +20,7 @@ public class King extends ChessPiece {
         } else if (owner.equalsIgnoreCase("player2")) {
             id = 'k';
         }
+        threateningPieces = new ArrayList<>();
     }
 
     /** Checks if more is valid for King, then moves the piece.
@@ -31,5 +34,61 @@ public class King extends ChessPiece {
             return checkLineOfSight(chessLocation, location) && super.moveTo(location);
         }
         return false;
+    }
+    @Override
+    public void updateThreateningLocation(ChessLocation newLocation) {
+
+    }
+
+    private ChessPiece locationInDanger(ChessLocation location) {
+
+
+    }
+
+    private void updateVertical(ChessLocation location, int one) {
+        ChessBoard board = chessGame.getChessBoard();
+        int inc = one;
+        while (location.getRow() + inc < 8 &&
+                location.getRow() + inc > 0) {
+
+            ChessPiece piece = board.getPieceAt(new ChessLocation(location.getRow() + inc, location.getCol()));
+            if (piece != null) {
+                if (piece.getOwner().equalsIgnoreCase(owner)) {
+                    return;
+                } else if (Character.toLowerCase(piece.getId()) == 'q' ||
+                            Character.toLowerCase(piece.getId()) == 'r' ||
+                            Character.toLowerCase(piece.getId()) == 'k' ||
+                            Character.toLowerCase(piece.getId()) == 'p') {
+
+                    threateningPieces.add(piece);
+                }
+            } else {
+                inc += one;
+            }
+        }
+    }
+
+    private void updateHorizontal(ChessLocation location, int one) {
+        ChessBoard board = chessGame.getChessBoard();
+        int inc = one;
+        while (location.getCol() + inc < 8 &&
+                location.getCol + inc > 0) {
+
+            ChessPiece piece = board.getPieceAt(new ChessLocation(location.getRow(), location.getCol() + inc));
+            if (piece != null) {
+                if (piece.getOwner().equalsIgnoreCase(owner)) {
+                    return;
+                } else if (Character.toLowerCase(piece.getId()) == 'q' ||
+                            Character.toLowerCase(piece.getId()) == 'r' ||
+                            Character.toLowerCase(piece.getId()) == 'k' ||
+                            Character.toLowerCase(piece.getId()) == 'p') {
+
+                    threateningPieces.add(piece);
+                }
+            } else {
+                inc += one;
+            }
+        }
+
     }
 }
